1aadea1e7de0c4ebb71885c92801a225d451ea9c
Vitor <metroid.p2p@gmail.com>
2016-05-07 16:01:16 -0300

    JS II Finalizado_

--- JS/JS II/js/block/card.js
+++ JS/JS II/js/block/card.js
@@ -1,12 +1,35 @@
+
+var $wrapCard = document.querySelector('.wrap-card');
 var $cardColors = document.querySelectorAll('.card-colors');
 
-for (var i = 0; i < $cardColors.length; i++) {
-	$cardColors[i].addEventListener('click', function() {
-		var $card = this.parentNode.parentNode.parentNode;
-		$card.dataset.color = this.dataset.color;
-		console.log($card.dataset.color);
-		console.log(this.dataset.color);
-		console.log(this.dataset);
-			
-	});	
-}
+	$wrapCard.addEventListener('click', function(event){
+		var $this = event.target;
+		var $card = $this.parentNode.parentNode.parentNode;
+		var $cardContent = $card.querySelector('.card-content');
+
+		if ($this.classList.contains('card-colors')) {	
+			$card.dataset.color = $this.dataset.color;
+
+			for (var j = 0; j < $cardColors.length; j++) {
+				$cardColors[j].classList.remove('isActive');
+			}
+		};
+
+		$this.classList.add('isActive');
+
+		if ($this.classList.contains('card_delete')) {
+			$card.remove();
+		};
+
+		if ($this.classList.contains('card_edit')) {
+			if ($cardContent.getAttribute('contenteditable') == 'false') {
+				$cardContent.setAttribute('contenteditable', 'true');
+				$cardContent.focus();
+				$this.classList.add('isActive');
+			} else {
+			$cardContent.setAttribute('contenteditable', 'false');
+			$cardContent.blur();
+			$this.classList.remove('isActive');
+			};
+		} 
+	});
--- PHP/Fundamentos da Web/v2/loja/conecta.php
+++ PHP/Fundamentos da Web/v2/loja/conecta.php
@@ -1,3 +1,2 @@
 <?php  
 	$conexao = mysqli_connect('localhost', 'root', '', 'loja');
-	//$conexao = mysqli_connect('localhost', 'adminbb7IvCP', '', 'StC42NPV_CIW');
