14ef6c0dba064e3a7d9d24add9eb447dd1042c52
Vitor <metroid.p2p@gmail.com>
2016-05-30 14:11:41 -0300

    angular

--- JS/Angular/public/js/directives/minhas-diretivas.js
+++ JS/Angular/public/js/directives/minhas-diretivas.js
@@ -2,16 +2,26 @@ angular.module('minhasDiretivas', [])
     .directive('meuPainel', function() {
 
         var ddo = {};
-
         ddo.restric = "AE";
-
         ddo.scope = {
             titulo: '@'
         };
 
         ddo.transclude = true;
-
         ddo.templateUrl = 'js/directives/meu-painel.html'
+
+        return ddo;
+    })
+    .directive('minhaFoto', function() {
+        var ddo = {};
+        ddo.restric = "AE";
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
+
+        ddo.transclude = true;
+        ddo.templateUrl = 'js/directives/minha-foto.html'
 
         return ddo;
     });
