213929d8ca46788d87d5342374f9e39956edbd5f
Vitor <metroid.p2p@gmail.com>
2016-08-21 19:56:57 -0300

    ecs6 aula 3

---
+++ JS/es6/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,27 @@
+class NegociacaoController {
+
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        // let data = new Date(this._inputData.value.replace('/-/g',','));
+        let data = new Date(...this._inputData.value.split('-').map(function(item, indice) {
+            return item - indice % 2
+            })
+        );
+
+        let negociacao = new Negociacao(
+                data,
+                this._inputQuantidade,
+                this._inputValor
+        );
+
+        console.log(data);
+    }
+}
