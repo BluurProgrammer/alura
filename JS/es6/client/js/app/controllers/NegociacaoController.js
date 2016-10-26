db596654286afc48883758e254df93b58a39a7cf
Vitor <metroid.p2p@gmail.com>
2016-10-25 22:32:57 -0200

    JS III

--- JS/es6/client/js/app/controllers/NegociacaoController.js
+++ JS/es6/client/js/app/controllers/NegociacaoController.js
@@ -1,27 +1,44 @@
 class NegociacaoController {
 
     constructor() {
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
+
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._mensagem.texto = 'Negociação Adicionada com Sucesso!';
+        this._mensagemView.update(this._mensagem);
+        this._limpaFormulario();
+    }
 
-        // let data = new Date(this._inputData.value.replace('/-/g',','));
-        let data = new Date(...this._inputData.value.split('-').map(function(item, indice) {
-            return item - indice % 2
-            })
-        );
+    _criaNegociacao() {
 
-        let negociacao = new Negociacao(
-                data,
-                this._inputQuantidade,
-                this._inputValor
-        );
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value);
+    }
 
-        console.log(data);
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
